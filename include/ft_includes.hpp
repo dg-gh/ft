16f98336aa60597b039c6d4a310a110de8cbc6e9
Dave <52504186+dg-gh@users.noreply.github.com>
2021-12-05 22:26:52 +0100

    Create ft_includes.hpp

---
+++ include/ft_includes.hpp
@@ -0,0 +1,27 @@
+#ifndef FT_INCLUDES_HPP
+#define FT_INCLUDES_HPP
+
+// get asio at : https://think-async.com/Asio/
+
+#include <cstdint>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <string_view>
+#include <iostream>
+#include <fstream>
+#include <list>
+#include <functional>
+#include <thread>
+#include <chrono>
+#include <random>
+#include <cassert>
+
+#include <filesystem>
+
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+#endif // FT_INCLUDES_HPP
